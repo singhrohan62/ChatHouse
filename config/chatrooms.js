2e0c6a6667642c018a1a66fbdc739a1f7b27f74f
Rohan Singh <rohaniiitv@gmail.com>
2018-08-04 14:30:33 +0530

    added chatrooms and houses

---
+++ config/chatrooms.js
@@ -0,0 +1,17 @@
+module.exports = [
+	
+	{
+		name : 'Hogwarts Hall',
+		image : 'https://2u878v2zya7t3dgei82fads5-wpengine.netdna-ssl.com/wp-content/uploads/2017/09/42a073e32a6d212114c0360827c964cd.jpg';
+	},
+
+	{
+		name : 'Diagon Alley',
+		image : 'https://timedotcom.files.wordpress.com/2017/05/170518-universal-studios-cost-wizarding-world-harry-potter-diagon-alley.jpg?quality=85'
+	},
+
+	{
+		name : 'Hogwarts Station',
+		image : 'https://hips.hearstapps.com/sev.h-cdn.co/assets/15/36/1024x687/gallery-1441130300-hogwartsexpress.png?resize=768:*'
+	}
+]
